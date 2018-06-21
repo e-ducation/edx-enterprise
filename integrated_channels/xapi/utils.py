ac763a0806cd7c6a7d9f2802d2cb9872aae0d945
Saleem Latif <saleem_ee@hotmail.com>
2018-06-21 13:12:36 +0500

    helper functins to send course enrollment event to lrs

--- integrated_channels/xapi/utils.py
+++ integrated_channels/xapi/utils.py
@@ -6,3 +6,18 @@ Utility functions for X-API.
 
 from __future__ import absolute_import, unicode_literals
 
+from integrated_channels.xapi.models import XAPILRSConfiguration
+from integrated_channels.xapi.client import EnterpriseXAPIClient
+from integrated_channels.xapi.statements.learner_course_enrollment import LearnerCourseEnrollmentStatement
+
+
+def send_xapi_statement_course_enrollment(lrs_configuration, course_enrollment):
+    """
+    Send X-API statement for course enrollment.
+
+    Arguments:
+         lrs_configuration (XAPILRSConfiguration): XAPILRSConfiguration instance where to send statements.
+         course_enrollment (CourseEnrollment): Course enrollment object.
+    """
+    statement = LearnerCourseEnrollmentStatement(course_enrollment.user, course_enrollment.course, {}, {})
+    EnterpriseXAPIClient(lrs_configuration).save_statement(statement)
