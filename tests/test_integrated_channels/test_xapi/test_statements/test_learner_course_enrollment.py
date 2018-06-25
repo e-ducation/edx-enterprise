535d05324d8d9c0070eb2055396dbd02773dfc37
Saleem Latif <saleem_ee@hotmail.com>
2018-06-25 13:56:54 +0500

    Updated tests for context

--- tests/test_integrated_channels/test_xapi/test_statements/test_learner_course_enrollment.py
+++ tests/test_integrated_channels/test_xapi/test_statements/test_learner_course_enrollment.py
@@ -13,7 +13,7 @@ from mock import Mock
 from pytest import mark
 
 from integrated_channels.xapi.statements.learner_course_enrollment import LearnerCourseEnrollmentStatement
-from test_utils import factories
+from test_utils import factories, TEST_UUID, TEST_COURSE
 
 
 @mark.django_db
@@ -30,40 +30,70 @@ class TestLearnerCourseEnrollmentStatement(unittest.TestCase):
         # pylint: disable=no-member
         self.course_overview = Mock(display_name=faker.text(max_nb_chars=25), short_description=faker.text())
 
+        self.user_details = {
+            'enrollment_created_timestamp': '2018-06-25T04:45:32.642094',
+            'enterprise_user_id': 1,
+            'lms_user_id': 2,
+            'enterprise_sso_uid': TEST_UUID,
+            'user_account_creation_date': '2017-09-27T02:51:32.612293',
+            'user_email': 'user@example.com',
+            'user_username': 'test_user',
+            'user_country_code': 'PK',
+            'user_current_enrollment_mode': 'verified',
+        }
+        self.course_details = {
+            'course_id': TEST_COURSE,
+            'course_title': self.course_overview.display_name,
+            'course_duration': '6 Months',
+            'course_min_effort': '3 days per week',
+        }
+
         self.expected = {
-            "verb":
+            'verb':
                 {
-                    "id": "http://adlnet.gov/expapi/verbs/registered",
-                    "display": {"en-US": "registered"}
+                    'id': 'http://adlnet.gov/expapi/verbs/registered',
+                    'display': {'en-US': 'registered'}
                 },
-            "version": "1.0.1",
-            "actor":
+            'version': '1.0.1',
+            'actor':
                 {
-                    "mbox": "mailto:{email}".format(email=self.user.email),
-                    "name": self.user.username,
-                    "objectType": "Agent"
+                    'mbox': 'mailto:{email}'.format(email=self.user.email),
+                    'name': self.user.username,
+                    'objectType': 'Agent'
                 },
-            "object":
+            'object':
                 {
-                    "definition":
+                    'definition':
                         {
-                            "description":
+                            'description':
                                 {
-                                    "en-US": self.course_overview.short_description
+                                    'en-US': self.course_overview.short_description
                                 },
-                            "name":
+                            'name':
                                 {
-                                    "en-US": self.course_overview.display_name
+                                    'en-US': self.course_overview.display_name
                                 }
                         },
-                    "id": "http://adlnet.gov/expapi/activities/course",
-                    "objectType": "Activity"
-                }
+                    'id': 'http://adlnet.gov/expapi/activities/course',
+                    'objectType': 'Activity'
+                },
+            'context':
+                {
+                    'extensions': {
+                        'http://id.tincanapi.com/extension/course-details': self.course_details,
+                        'http://id.tincanapi.com/extension/user-details': self.user_details
+                    }
+                },
         }
 
     def test_statement(self):
         """
         Validate statement when learner enrolls in a course.
         """
-        statement = LearnerCourseEnrollmentStatement(self.user, self.course_overview)
+        statement = LearnerCourseEnrollmentStatement(
+            self.user,
+            self.course_overview,
+            self.user_details,
+            self.course_details,
+        )
         assert json.loads(statement.to_json()) == self.expected
